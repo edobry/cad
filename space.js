05eae2d74a7a6b4dd1c54cb3605103cffbc72537
Eugene Dobry <eugene@dobry.me>
2015-03-07 15:32:05 -0500

    fixed setting

--- space.js
+++ space.js
@@ -1,7 +1,7 @@
 var exports = {};
 
-var traverseDo = function(space, func, bounds) {
-	var measureDimension = function(space, dim) {
+var traverseDo = function(func, bounds) {
+	var measureDimension = function(dim) {
 		var incr = function(val) {
 			return function() {
 				val++;
@@ -9,47 +9,24 @@ var traverseDo = function(space, func, bounds) {
 		};
 
 		var size = 0;
-		traverseDo(space, incr(size), [1, space[0].length]);
+		this.traverseDo(incr(size), [1, space[0].length]);
 		return size;
 	};
 	var getBound = function(dim) {
 		return (bounds && bounds[dim]
 					? bounds[dim]
-					: measureDimension(space, dim)
+					: measureDimension.bind(this)(dim)
 			   );
-	};
-	var setter = function(pos) {
-		return function(val) {
-			space[pos[0]][pos[1]] = val;
-		};
-	};
-	var getter = function(pos) {
-		return function() {
-			return space[pos[0]][pos[1]];
-		};
-	};
-	var visit = function(pos) {
-		return {
-			get: getter(pos),
-			set: setter(pos)
-		};
 	};
 
 	for(i = 0; i < getBound(0); i++) {
-		if(!space[i])
-			space[i] = [];
+		if(!this.rows[i])
+			this.rows[i] = [];
 
 		for(j = 0; j < getBound(1); j++) {
-			func(visit([i,j]));
+			func(this.visit([i,j]));
 		}
 	}
-	return space;
-};
-
-var set = function(val) {
-	return function(point) {
-		point.set(val);
-	};
 };
 
 var toString = function() {
@@ -59,9 +36,31 @@ var toString = function() {
 	return this.rows.map(printRow).join('\n');
 };
 
+var visit = function(pos) {
+	var self = this;
+	return {
+		get: function() {
+			return self.rows[pos[0]][pos[1]];
+		},
+		set: function(val) {
+			self.rows[pos[0]][pos[1]] = val;
+		}
+	};
+};
+
+var set = function(val) {
+	return function(point) {
+		point.set(val);
+	};
+};
+
 var Space = function(dimensions, val) {
-	this.rows = traverseDo([], set(val), dimensions);
+	this.rows = [];
 	this.toString = toString.bind(this);
+	this.visit = visit.bind(this);
+	this.traverseDo = traverseDo.bind(this);
+	
+	this.traverseDo(set(val), dimensions);
 };
 
 module.exports = Space;
